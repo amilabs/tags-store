38654947f9db0744e5e60202a93ab1ed9de3a735
Anton Tursenev <anton@rikishi.info>
2020-03-27 16:48:29 +0700

    Update utils

--- src/utils.js
+++ src/utils.js
@@ -1,22 +1,5 @@
 export { default as uniqBy } from 'lodash/uniqBy'
-
-export function isEmpty (data) {
-  return [Object, Array].includes((data || {}).constructor) && !Object.entries((data || {})).length
-}
-
-export function omit (data, keys) {
-  return Object.keys(data)
-    .filter(key => keys.indexOf(key) === -1)
-    .reduce((newObj, key) => Object.assign(newObj, { [key]: data[key] }), {})
-}
-
-export function pickBy (data, func) {
-  return Object.keys(data)
-    .filter(key => func(data[key]))
-    .reduce((newObj, key) => Object.assign(newObj, { [key]: data[key] }), {})
-}
-
-export function mapValues (data, func) {
-  return Object.keys(data)
-    .reduce((newObj, key) => Object.assign(newObj, { [key]: func(data[key], key) }), {})
-}
+export { default as isEmpty } from 'lodash/isEmpty'
+export { default as omit } from 'lodash/omit'
+export { default as pickBy } from 'lodash/pickBy'
+export { default as mapValues } from 'lodash/mapValues'
